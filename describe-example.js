646eb2fb1ea8335c9c6201f78377ad5dcc72be59
ciju <ciju.ch3rian@gmail.com>
2016-09-01 18:10:49 +0530

    Rename `it` to `test`

--- describe-example.js
+++ describe-example.js
@@ -3,7 +3,7 @@ var tfns = require('./describe-compiled.js').default,
   describe = tfns.describe,
   setup = tfns.setup,
   teardown = tfns.teardown,
-  it = tfns.it;
+  test = tfns.it;
 
 function log() {
   console.log.apply(console, arguments);
@@ -12,7 +12,7 @@ function log() {
 var obj = {};
 describe('True Or False? ', function () {
   describe('setup', function () {
-    it('should setup num', function () {
+    test('should setup num', function () {
       assert.equal(obj.num, 2);
     });
     setup(function () {
@@ -24,39 +24,39 @@ describe('True Or False? ', function () {
   });
 
   describe('teardown', function () {
-    it('should teardown num', function () {
+    test('should teardown num', function () {
       assert.equal(obj.num, null);
     });
   });
 
   describe('truthy => ', function () {
-    it('empty array', function () {
+    test('empty array', function () {
       assert.equal(!![0], true);
     });
 
-    it('empty object', function () {
+    test('empty object', function () {
       assert.equal(!!{}, true);
     });
   });
 
   describe('falsy => ', function () {
     describe('undefined & nil', function () {
-      it('undefined', function () {
+      test('undefined', function () {
         assert.equal(!(void 0), true);
       });
-      it('null', function () {
+      test('null', function () {
         assert.equal(!null, true);
       });
     });
 
-    it('should test ![] === true ', () => {
+    test('should test ![] === true ', () => {
       assert.equal(![], true);
     });
 
-    it('!NaN === true', function () {
+    test('!NaN === true', function () {
       assert.equal(!NaN, true);
     });
-    it('!(empty string) === true', function () {
+    test('!(empty string) === true', function () {
       assert.equal(!'', true);
     });
   });
